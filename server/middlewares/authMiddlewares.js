4136461b904095ee062207aab7e0e894ef5aaf25
Manthan Singla <manthan29singla@gmail.com>
2026-04-12 18:03:44 +0530

    Resolve the HTTP status code errorrs

--- server/middlewares/authMiddlewares.js
+++ server/middlewares/authMiddlewares.js
@@ -6,12 +6,12 @@ const authMiddleware= async (req, res, next)=>{
     try{
         const authHeader= req.headers.authorization;
         if(!authHeader || !authHeader.startsWith('Bearer ')){
-            return res.status(404).json({message: "Authorization failed", success: false});
+            return res.status(401).json({message: "Authorization failed", success: false});
         }
 
         const token= authHeader.split(' ')[1];
         if(!token){
-            return res.status(404).json({success: false, message: "Token missing"})
+            return res.status(401).json({success: false, message: "Token missing"})
         }
 
         // Verify Token
@@ -22,11 +22,11 @@ const authMiddleware= async (req, res, next)=>{
         const user = await User.findById(userId);
 
         if(!user){
-            return res.status(404).json({success: false, message: "User not found"});
+            return res.status(401).json({success: false, message: "User not found"});
         }
 
         if(!user.isActive){
-            return res.status(404).json({success: false, message: "Account is deactivated. Please reactivate/login to continue."});
+            return res.status(403).json({success: false, message: "Account is deactivated. Please reactivate/login to continue."});
         }
 
         return next();
