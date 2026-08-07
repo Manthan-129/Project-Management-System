c9bc06a5c292fb118fc8fd2ec0e3a8a73ed1bea6
Manthan Singla <manthan29singla@gmail.com>
2026-08-07 12:19:38 +0530

    MongoDB Connections

--- server/configs/db.js
+++ server/configs/db.js
@@ -3,7 +3,17 @@ const mongoose = require('mongoose');
 
 const connectDB= async ()=>{
     try{
-        await mongoose.connect(process.env.MONGO_URL);
+        await mongoose.connect(process.env.MONGO_URL, {
+            maxPoolSize: 10,
+            socketTimeoutMS: 45000,
+            connectTimeoutMS: 10000,
+        });
+        mongoose.connection.on('error', (err) => {
+            console.error('MongoDB connection error:', err);
+        });
+        mongoose.connection.on('disconnected', () => {
+            console.warn('MongoDB disconnected. Attempting reconnect...');
+        });
         console.log("MongoDB connected successfully!!");
     }catch(error){
         console.log("Error connecting to MongoDB: ", error);
